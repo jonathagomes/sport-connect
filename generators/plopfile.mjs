e414c0de85dc2f0864957c8c2ee3f79bb3a9f099
Jonatha Gomes <jonatha.dev.js@gmail.com>
2023-01-24 10:01:51 -0300

    feat: add plop to project and config paths

---
+++ generators/plopfile.mjs
@@ -0,0 +1,22 @@
+export default (
+  /** @type {import('plop').NodePlopAPI} */
+  plop,
+) => {
+  plop.setGenerator("component", {
+    description: "Create a component",
+    prompts: [
+      {
+        type: "input",
+        name: "name",
+        message: "What is your component name?",
+      },
+    ],
+    actions: [
+      {
+        type: "add",
+        path: "../src/components/{{pascalCase name}}/index.tsx",
+        templateFile: "templates/Component.tsx.hbs",
+      },
+    ],
+  });
+};
